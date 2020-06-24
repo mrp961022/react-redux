1a106f4aadb769a12a5bbca7d8e2a59bb1909ca0
maruiping <13833589337@163.com>
2020-06-24 17:26:23 +0800

    提交一下

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,12 @@
 import React,{ Component } from 'react';
+import "./index.less"
 
 export default class Footer extends Component{
     render(){
         return(
-            <div>Footer</div>
+            <div className="footer">
+                版权所有: 这是一段版权文字
+            </div>
         )
     }
 }
